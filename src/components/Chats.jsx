208b97b5d827aa4dbf9fb5a18325f22c31acb9c2
DEADHEAD652 <hamzajjmalik@gmail.com>
2023-04-01 16:48:00 +0500

    auth context

---
+++ src/components/Chats.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useHistory } from 'react-router-dom'
+import {ChatEngine} from 'react-chat-engine'
+import { auth } from './Firebase'
+
+const Chats = () => {
+  return (
+    <div>Chats</div>
+  )
+}
+
+export default Chats
